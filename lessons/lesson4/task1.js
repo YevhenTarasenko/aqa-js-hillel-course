a3199f6a083de5bdcdd6e5aa0e219285c8e4ce15
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2024-04-28 13:52:53 +0300

    Commit: Folder lesson4 with homework #4  was created

---
+++ lessons/lesson4/task1.js
@@ -0,0 +1,15 @@
+// Завдання 1: Оголошення змінних для примітивних типів
+
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined.Присвойте їм значення та виведіть кожну змінну на консоль.
+
+let myString = "This is a string";
+let myNumber = 5;
+let myBoolean = true;
+let myUndefined;
+let myNull = null;
+
+console.log(`This is a string - ${myString}`);
+console.log(`This is a number - ${myNumber}`);
+console.log(`This is a boolean - ${myBoolean}`);
+console.log(`This is undefined - ${myUndefined}`);
+console.log(`This is null - ${myNull}`);
