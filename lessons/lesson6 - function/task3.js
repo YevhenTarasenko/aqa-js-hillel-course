8ce5015bb6c47789601676ac318f9f0b15fbbe4f
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2024-05-04 19:25:51 +0300

    Commit: Folder lesson6 with homework #6 (functions) was created

---
+++ lessons/lesson6 - function/task3.js
@@ -0,0 +1,29 @@
+// Завдання 3
+
+// Створіть функцію checkOrder яка перевіряє можливість оформлення замовлення і повертає повідомлення про результат.Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+// 	available - доступна кількість товарів на складі
+// ordered - кількість одиниць товару в замовленні
+// Якщо товарів в наявності менше ніж замовлено — повернути рядок з повідомленням - “Your order is too large, we don’t have enough goods.”
+// Якщо ordered дорівнює нулю — повернути рядок з повідомленням - “Your order is empty”
+// У разі якщо товарів на складі вистачає — повернути рядок з повідомленням - “Your order is accepted”
+
+function checkOrder(ordered, available) {
+	if (typeof ordered !== 'number' || typeof available !== 'number' || ordered < 0 || available < 0) {
+		return "Error: Both ordered and available must be numbers and non-negative"
+	}
+	if (ordered == 0) {
+		return "Your order is empty."
+	}
+	if (ordered > available) {
+		return "Your order is too large, we don't have enough goods."
+	}
+	return "Your order is accepted."
+}
+
+console.log(checkOrder(10, 8)) //"Your order is too large, we don't have enough goods"
+console.log(checkOrder(0, 8)) //"Your order is empty"
+console.log(checkOrder(2, 8)) //"Your order is accepted"
+console.log(checkOrder(-2, 8)) //"Error: Both ordered and available must be numbers and non-negative"
+console.log(checkOrder(2, -8)) //"Error: Both ordered and available must be numbers and non-negative"
+console.log(checkOrder("test", 8)) //"Error: Both ordered and available must be numbers and non-negative"
+console.log(checkOrder(8, "test")) //"Error: Both ordered and available must be numbers and non-negative"
